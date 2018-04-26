d9e888f8b762c6ef4d3e92fc5f9c08f004b2f584
Andy Turner <a.turner@epcc.ed.ac.uk>
2018-04-26 14:18:10 +0100

    Updated batch guide

---
+++ user-guide/example_array_impi.bash
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+
+# PBS job options
+#PBS -N ArrayJob
+#PBS -l select=16
+#PBS -l walltime=4:0:0
+#PBS -J 1-56
+
+# Replace [budget code] below with your project code (e.g. t01)
+#PBS -A [budget code]
+
+cd ${PBS_O_WORKDIR}
+
+# Load any required modules
+module load intel-tools-18
+
+# Run with array index as the first argument to the executable
+mpirun -n 1536 -ppn 24 ./my_mpi_executable.x $PBS_ARRAY_INDEX
+
